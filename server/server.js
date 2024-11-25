b56d61606b4f507193509b339eea337cae58db0e
Ľubomír <skgametolator@gmail.com>
2024-11-25 20:40:54 +0100

    Pridané prepojenie s PostgreSQL databázou

--- server/server.js
+++ server/server.js
@@ -1,8 +1,22 @@
 const express = require('express')
-const app = express()
+const pool = require('./db');
 
-app.get("/api", (req, res) => {
-    res.json({"users": ["userOne", "userTwo", "userThree"] })
-})
+
+const app = express();
+
+app.use(express.json());
+
+app.get('/api/polozka', async (req, res) => {
+    try {
+        const result = await pool.query('SELECT * FROM polozka');
+    } catch (err) {
+        console.error(err.message);
+        res.status(500).send('Server Error');
+    }
+});
+
+// app.get("/api", (req, res) => {
+//     res.json({"users": ["userOne", "userTwo", "userThree"] })
+// })
 
 app.listen(5000, () => {console.log("Server started on port 5000")})
